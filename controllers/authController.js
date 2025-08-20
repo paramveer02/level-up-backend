9adc0416fce20dd0cdac536d5d20bf16ba16ae0a
Paramveer <paramveermarwahafc@gmail.com>
2025-08-20 10:03:22 +0200

    feature: authentication request handlers (in progress)

---
+++ controllers/authController.js
@@ -0,0 +1,6 @@
+import User from "../models/User";
+import { asyncWrapper } from "../utils/asyncWrapper.js";
+
+export const signup = asyncWrapper(async function (req, res) {});
+export const login = asyncWrapper(async function (req, res) {});
+export const logout = asyncWrapper(async function (req, res) {});
