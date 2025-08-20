9735dd6a8e67a9d245ae77e09584f5c93ab22fec
Paramveer <paramveermarwahafc@gmail.com>
2025-08-20 09:55:36 +0200

    feature: setup express app; security headers; cors whitelist & app middlewares

---
+++ app.js
@@ -0,0 +1,46 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import helmet from "helmet";
+import notFound from "./middlewares/notFound.js";
+import errorHandler from "./middlewares/errorHandler.js";
+
+export const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// CORS
+const allowedOrigins = [process.env.CLIENT_URL, "http://localhost:5173"].filter(
+  Boolean
+);
+
+app.use(
+  cors({
+    origin: (origin, cb) => {
+      if (!origin || allowedOrigins.includes(origin)) return cb(null, true);
+      return cb(new Error("Not allowed by cors"));
+    },
+    credentials: true,
+  })
+);
+
+// security headers
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+
+// middlewares
+app.use(express.json({ limit: "10kb" }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+// routes
+app.get("/api/v1/test", (req, res) => {
+  res.send("TESTING successful!");
+});
+
+// error handler
+app.use(notFound);
+app.use(errorHandler);
