344f9f8bd35144ed4bc60f1606b181592ef34165
Paramveer <paramveermarwahafc@gmail.com>
2025-08-21 13:53:27 +0200

    feature: routes to access ai endpoints

---
+++ routes/aiRouter.js
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import { aiPing, aiPlan } from "../controllers/aiController.js";
+import { authenticate } from "../middlewares/authenticate.js";
+// If you want to require login later: import authenticate from '../middlewares/authenticate.js';
+
+const aiRouter = Router();
+
+// public for first test:
+aiRouter.get("/ping", aiPing);
+aiRouter.post("/plan", authenticate, aiPlan);
+
+// later you can protect it with auth:
+// r.post('/plan', authenticate, aiPlan);
+
+export default aiRouter;
