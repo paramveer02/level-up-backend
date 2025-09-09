c7ca9e7d694e07fe8f1022c76f635f555d5e03ef
Cirooochen <cirochen0406@gmail.com>
2025-09-09 11:02:43 +0200

    changed habitbase weight for workout act

--- dev-data/habitBase.js
+++ dev-data/habitBase.js
@@ -11,7 +11,7 @@ export const habitBase = [
     id: "strength-training",
     name: "30-min Strength Training",
     category: "exercise",
-    weight: 5,
+    weight: 4,
     emoji: "💪",
   },
   {
